d05e4c2012ccdc96ffa5c50b2cba3b90b2330f6c
Vadim Brk <Vadim.Brk@outlook.com>
2023-06-29 21:41:39 +0200

    Added RRD

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+
 import Header from 'components/Header';
+import { Home, NotFound } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="container">
-        app
-      </div>
+      <Header/>
+      <Routes>
+        <Route path={'/'} element={<Home/>}/>
+        <Route path={'*'} element={<NotFound/>}/>
+      </Routes>
     </div>
   );
 }
