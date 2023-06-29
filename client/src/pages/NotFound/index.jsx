efc9bf6078a5b65df09bbf3a2680a89f8a329b22
Vadim Brk <Vadim.Brk@outlook.com>
2023-06-29 21:41:22 +0200

    Added NotFound page

---
+++ client/src/pages/NotFound/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import LinkButton from 'components/LinkButton';
+
+const NotFound = () => {
+  return (
+    <div className="container not_found">
+      <h2>Page Not found!</h2>
+      <LinkButton path={'/'}>🏇 to Main Page</LinkButton>
+    </div>
+  );
+};
+
+export default NotFound;
