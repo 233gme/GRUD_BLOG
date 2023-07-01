2a7d02eb9e40918c11b1963575e3ab036fea3b50
Vadim Brk <Vadim.Brk@outlook.com>
2023-07-01 20:45:38 +0200

    Added slices for posts

---
+++ client/src/shared/config/redux/slices/posts.js
@@ -0,0 +1,57 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'shared/config/axios/axios';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const { data } = await axios.get('/posts');
+  return data;
+});
+
+export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
+  const { data } = await axios.get('/posts/tags');
+  return data;
+});
+
+const initialState = {
+  posts: {
+    items: [],
+    status: 'loading'
+  },
+  tags: {
+    items: [],
+    status: 'loading'
+  },
+};
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducer: {},
+  extraReducers: {
+    [fetchPosts.pending]: (state, action) => {
+      state.posts.items = [];
+      state.posts.status = 'loading';
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = 'loaded';
+    },
+    [fetchPosts.rejected]: (state, action) => {
+      state.posts.items = [];
+      state.posts.status = 'error';
+    },
+    [fetchTags.pending]: (state, action) => {
+      state.tags.items = [];
+      state.tags.status = 'loading';
+    },
+    [fetchTags.fulfilled]: (state, action) => {
+      state.tags.items = action.payload;
+      state.tags.status = 'loaded';
+    },
+    [fetchTags.rejected]: (state, action) => {
+      state.tags.items = [];
+      state.tags.status = 'error';
+    }
+  }
+});
+
+export const postsReducer = postsSlice.reducer;
