e234b416151ce3ef39781cd4cd9213d6d6e486ce
Vadim Brk <Vadim.Brk@outlook.com>
2023-07-01 15:46:05 +0200

    Added Mobile menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import { useTheme } from 'shared/providers';
 import Header from 'widgets/Header';
-import AppRouter from './shared/providers/AppRouter/AppRouter';
+import AppRouter from 'shared/providers/AppRouter/AppRouter';
+import useElementWidth from 'shared/hoooks/useElementWidth/useElementWidth';
 
 function App() {
   const { theme } = useTheme();
+  const [ref, width] = useElementWidth();
 
   return (
-    <div className={`App ${theme}`}>
-      <Header/>
+    <div className={`App ${theme}`} ref={ref}>
+      <Header width={width}/>
       <AppRouter/>
     </div>
   );
--- client/src/widgets/Header/index.jsx
+++ client/src/widgets/Header/index.jsx
@@ -1,21 +1,44 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 import Button from 'shared/ui/Button';
 import ThemeSwitcher from 'widgets/ThemeSwitcher';
 import LangSwitcher from 'widgets/LangSwitcher';
+import IconButton from 'shared/ui/IconButton';
+import { ReactComponent as MenuIcon } from 'shared/assets/icons/menu.svg';
 
-function Header() {
+const Header = ({ width }) => {
+  const [mobile, setMobile] = useState(false);
+  const [hideMenu, setHideMenu] = useState(true);
+  const handleMenu = () => setHideMenu(!hideMenu);
+
+  useEffect(() => {
+    if (1024 > width) {
+      setMobile(true);
+      setHideMenu(true);
+    } else {
+      setMobile(false);
+    }
+  }, [width]);
+
+  const headerBtnClass = `btn_block ${mobile ? hideMenu ? 'mobile_btn_block' : '' : ''}`;
+
   return (
     <div className="header container">
       <Link to="/"><h1>BLOG</h1></Link>
-      <div className="btn_block">
+      <div className={headerBtnClass}>
         <LangSwitcher/>
         <ThemeSwitcher/>
         <Button type={'pink'}>{'SingUp'}</Button>
         <Button>{'LogIn'}</Button>
       </div>
+      <div className="menu_button">
+        <IconButton action={handleMenu}>
+          <MenuIcon/>
+        </IconButton>
+      </div>
     </div>
   );
-}
+};
 
 export default Header;
