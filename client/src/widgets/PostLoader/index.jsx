073a742c40e6b7649113d973780ca8ebb847de90
Vadim Brk <Vadim.Brk@outlook.com>
2023-07-01 18:01:45 +0200

    Added post loader widget

---
+++ client/src/widgets/PostLoader/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import Loader from 'shared/ui/Loader';
+
+const PostLoader = () => {
+  return (
+    <div className={'post_loader'}>
+      <Loader/>
+    </div>
+  );
+};
+
+export default PostLoader;
