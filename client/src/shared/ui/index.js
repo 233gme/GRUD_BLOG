f5d37bd8a0b5cae206525579de4701f9b7593b15
Vadim Brk <Vadim.Brk@outlook.com>
2023-07-02 14:51:16 +0200

    Added public api

---
+++ client/src/shared/ui/index.js
@@ -0,0 +1,8 @@
+import Button from './Button';
+import IconButton from './IconButton';
+import Input from './Input';
+import LinkButton from './LinkButton';
+import Loader from './Loader';
+import Image from './Image';
+
+export { Button, IconButton, Input, LinkButton, Loader, Image };
