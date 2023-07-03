01293c7453af1d8d82f2a23ee4385ec3f9ee63ed
Vadim Brk <Vadim.Brk@outlook.com>
2023-07-03 02:38:13 +0200

    Updated full post component

--- client/src/pages/FullPost/index.jsx
+++ client/src/pages/FullPost/index.jsx
@@ -1,14 +1,18 @@
-import { useParams } from 'react-router-dom';
+import './styles.css';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'shared/config/axios/axios';
 import PageLoader from 'widgets/PageLoader';
 import ReactMarkdown from 'react-markdown';
-import { Image } from '../../shared/ui';
+import { IconButton, Image, PostDesc } from 'shared/ui';
+import { ReactComponent as BackIcon } from 'shared/assets/icons/up-left.svg';
 
 const FullPost = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
+  const navigate = useNavigate();
+  const onReturn = () => navigate('/');
 
   useEffect(() => {
     axios.get(`/posts/${id}`)
@@ -27,13 +31,30 @@ const FullPost = () => {
   }
 
   return (
-    <div className={'container'}>
-      <Image src={data.imageUrl} alt={data.title}/>
+    <div className={'container full_post'}>
       <h2>{data.title}</h2>
-      <ReactMarkdown>
-        {data.text}
-      </ReactMarkdown>
-      <p>{data.text}</p>
+      <Image src={data.imageUrl} alt={data.title}/>
+      <ReactMarkdown>{data.text}</ReactMarkdown>
+      {
+        data.tags.length
+          ? (
+            <div className={'full_post_tags_block'}>
+              {
+                data.tags.map((tag, index) => (<span key={index}>{tag}</span>))
+              }
+            </div>
+          ) : null
+      }
+      <div className={'full_post_footer'}>
+        <PostDesc
+          viewsCount={data.viewsCount}
+          createdAt={data.createdAt}
+          userName={data.user.fullName}
+        />
+        <IconButton action={onReturn}>
+          <BackIcon/>
+        </IconButton>
+      </div>
     </div>
   );
 };
