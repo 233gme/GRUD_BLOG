75ccdeff735040792d4cd57c1f169d48cc588cb5
Vadim Brk <Vadim.Brk@outlook.com>
2023-07-03 02:38:45 +0200

    Updated generateTagsFromString function

--- client/src/shared/lib/generateTagsFromString/generateTagsFromString.js
+++ client/src/shared/lib/generateTagsFromString/generateTagsFromString.js
@@ -1,11 +1,13 @@
 export const generateTagsFromString = (string) => {
   if (string && string.length) {
-    return string.split(/\ |\+|\-|\*|\/|\#|\,|\.|\_/).map(item => {
-      const tag = item.trim();
-      if (tag.length) {
-        return `#${item}`;
-      }
-    });
+    return string.split(/\ |\+|\-|\*|\/|\#|\,|\.|\_/)
+      .filter(Boolean)
+      .map(item => {
+        const tag = item.trim();
+        if (tag.length) {
+          return `#${item}`;
+        }
+      });
   } else {
     return [];
   }
